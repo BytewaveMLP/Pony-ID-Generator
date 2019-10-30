734812391515e4d8a4a48c2b176a6d6f0399f1d5
Eliot Partridge <shortcircuit369@gmail.com>
2019-10-29 23:17:13 -0500

    More style work, render name in correct place

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+/**
+ * Copyright (C) Eliot Partridge, 2019. Licensed under MIT.
+ */
+
 // ID card dimensions = 92mm * 60 mm
 // Apsect ratio = 1.533 : 1
 
@@ -9,8 +13,15 @@ window.addEventListener('DOMContentLoaded', () => {
 	const ctx = canvas.getContext('2d');
 	if (!(ctx instanceof CanvasRenderingContext2D)) return;
 
+	const bg = new Image();
+	let bgPattern = null;
+	bg.onload = () => {
+		bgPattern = ctx.createPattern(bg, 'no-repeat');
+		drawBox();
+	}
+	bg.src = './MLP-ID-Card.png';
+
 	const drawBox = () => {
-		ctx.fillStyle = '#222';
 		ctx.beginPath();
 		// top left
 		ctx.arc(BORDER_RADIUS, BORDER_RADIUS, BORDER_RADIUS, Math.PI, 3 * Math.PI / 2);
@@ -22,36 +33,34 @@ window.addEventListener('DOMContentLoaded', () => {
 		ctx.arc(BORDER_RADIUS, canvas.height - BORDER_RADIUS, BORDER_RADIUS, Math.PI / 2, Math.PI);
 		// finish path
 		ctx.lineTo(0, BORDER_RADIUS);
+
+		ctx.fillStyle = bgPattern || '#222';
 		ctx.fill();
 	}
 
 	drawBox();
 
 	const redrawCard = () => {
-		const name = document.querySelector('#char-name').value;
-		const age = document.querySelector('#age').value;
+		const name = document.querySelector('#char-name').value.toUpperCase();
+		const eyeColor = document.querySelector('#eye-color').value.toUpperCase();
+		const city = document.querySelector('#city').value.toUpperCase() + ', EQUESTRIA';
 
 		ctx.clearRect(0, 0, canvas.width, canvas.height);
 
 		drawBox();
 
-		ctx.fillStyle = 'white';
-		ctx.font = '100pt \'Patrick Hand\',sans-serif';
+		ctx.fillStyle = '#060E21';
+		ctx.font = '77pt \'Patrick Hand\',sans-serif';
 		ctx.textBaseline = 'alphabetic';
 
-		ctx.fillText(name, 150, 150);
-		ctx.strokeStyle = 'orange';
-		ctx.lineWidth = 4;
-		ctx.strokeText(name, 150, 150);
-		ctx.fillStyle = 'black';
-		ctx.fillText(age, 70, 170);
+		ctx.fillText(name, 1.41*380, 1.41*265, 500);
 	};
 
-	document.querySelectorAll('input').forEach(input => {
-		input.addEventListener('change', redrawCard);
+	document.querySelectorAll(['input, select']).forEach(input => {
+		input.addEventListener('input', redrawCard);
 	});
 
-	document.querySelector('#save').addEventListener('click', e => {
+	document.querySelector('#save').addEventListener('click', () => {
 		const link = document.createElement('a');
 		const imgData = canvas.toDataURL('image/png').replace('image/png', 'image/octet-stream');
 		link.setAttribute('download', 'id-card.png');
